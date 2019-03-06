a020ba2e64702b93dc53d7298d8a812fd7b40ca9
Simon Hofmann <simon.hofmann@consol.de>
2019-03-06 09:52:17 +0100

    Added gnome-calculator for e2e sceanrios

--- whale-nut-ci/src/ubuntu/install/tools.sh
+++ whale-nut-ci/src/ubuntu/install/tools.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update
-apt-get install -y vim wget net-tools locales bzip2 build-essential libxt-dev libxtst-dev libpng++-dev libxinerama-dev -y
+apt-get install -y vim wget net-tools locales bzip2 build-essential libxt-dev libxtst-dev libpng++-dev libxinerama-dev gnome-calculator -y
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
