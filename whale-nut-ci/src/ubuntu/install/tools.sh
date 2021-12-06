f0fe96b1c0b1bced2c39c7d760d68b8e9a9fa205
Simon Hofmann <mail@simon-hofmann.org>
2021-12-06 15:47:12 +0100

    Use gcc-9 and g++-9

--- whale-nut-ci/src/ubuntu/install/tools.sh
+++ whale-nut-ci/src/ubuntu/install/tools.sh
@@ -4,8 +4,13 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update
-apt-get install -y vim wget net-tools locales bzip2 build-essential libxt-dev libxtst-dev gnome-calculator -y
+apt-get install software-properties-common -y
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y vim wget net-tools locales bzip2 build-essential libxt-dev libxtst-dev gnome-calculator gcc-9 g++-9 -y
 apt-get clean -y
+
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 echo "generate locales für en_US.UTF-8"
 locale-gen en_US.UTF-8
